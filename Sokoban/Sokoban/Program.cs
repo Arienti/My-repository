c5c29b9d857604a0ee4bbc507b8b43d22c574173
Denys Kirin <kirindenis1977@gmail.com>
2021-10-14 21:01:12 +0300

    Game engine is started

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -2,11 +2,49 @@
 
 namespace Sokoban
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 10;
+            int y = 10;
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.Write(" ");
+
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    y--; //X Y Z  У оо 
+                }
+                else
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    y++;
+                }
+                else
+                if (key.Key == ConsoleKey.LeftArrow)
+                {
+                    x--; 
+                }
+                else
+                if (key.Key == ConsoleKey.RightArrow)
+                {
+                    x++;
+                }
+
+                Console.CursorLeft = x;
+                Console.CursorTop = y;
+                Console.Write("X");
+
+                //Console.Write(key.KeyChar.ToString().ToUpper());                
+            }
+
+            Console.ReadLine();
         }
     }
 }

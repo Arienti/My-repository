a3bdf712cb653fe34407a3df57ae80339850d83f
Denys Kirin <kirindenis1977@gmail.com>
2021-10-21 21:46:52 +0300

    Snake game engine first version

--- Sokoban/Snake/Program.cs
+++ Sokoban/Snake/Program.cs
@@ -1,46 +1,166 @@
-﻿using System;
+﻿using System; //the System is name of the Unit 
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake
 {
+    public class AppleCoord //scheme 
+    {
+        public bool ImEating = false;
+        public int x; 
+        public int y;
+    }
+
+    public class TailSegmentCoord //scheme 
+    {        
+        public int x;
+        public int y;
+    }
+
     class Program
     {
+        static int widht = 80;
+        static int height = 40;
+        static int appleSize = 50;
+        static int eatedApplesCount = 0;
+        //{} () [] <>
+        //we use List of objects of class AppleCoord
+        static List<AppleCoord> apples = new List<AppleCoord>();
+        
+        static List<TailSegmentCoord> tail = new List<TailSegmentCoord>();
         private static void Main(string[] args)
         {
-            int x = 10;
-            int y = 10;
+            Random r = new Random();
+            for(int i=0; i < appleSize; i++ )
+            {
+                AppleCoord a = new AppleCoord();
+                a.x = r.Next(widht);
+                a.y = r.Next(height);
+                apples.Add(a);
 
+                Console.CursorLeft = a.x;
+                Console.CursorTop = a.y;
+                Console.Write("0");
+            }
+
+            TailSegmentCoord head = new TailSegmentCoord();
+            head.x = 10;
+            head.y = 10;
+            tail.Add(head);
+
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
             while (true)
             {
-                ConsoleKeyInfo key = Console.ReadKey();
+                if (Console.KeyAvailable)
+                {
+                    key = Console.ReadKey();
+                }
 
-                Console.CursorLeft = x;
-                Console.CursorTop = y;
+                Console.CursorLeft = tail[tail.Count - 1].x;
+                Console.CursorTop = tail[tail.Count - 1].y;
                 Console.Write(" ");
 
+                TailSegmentCoord storePrevSegment = new TailSegmentCoord();
+                storePrevSegment.x = tail[0].x;
+                storePrevSegment.y = tail[0].y;
+
+                for (int i = 1; i < tail.Count; i++)
+                {
+                    TailSegmentCoord storeCurrentSegment = new TailSegmentCoord();
+                    storeCurrentSegment.x = tail[i].x;
+                    storeCurrentSegment.y = tail[i].y;
+
+                    tail[i].x = storePrevSegment.x;
+                    tail[i].y = storePrevSegment.y;
+                    storePrevSegment = storeCurrentSegment;
+                }
+
+
+                //we check where snake go
                 if (key.Key == ConsoleKey.UpArrow)
                 {
-                    y--; //X Y Z  У оо 
+                    tail[0].y--; //X Y Z  У оо 
                 }
                 else
                 if (key.Key == ConsoleKey.DownArrow)
                 {
-                    y++;
+                    tail[0].y++;
                 }
                 else
                 if (key.Key == ConsoleKey.LeftArrow)
                 {
-                    x--;
+                    tail[0].x--;
                 }
                 else
                 if (key.Key == ConsoleKey.RightArrow)
                 {
-                    x++;
+                    tail[0].x++;
+                }
+                //at this stage our snake is make new step
+                
+                for (int i = 0; i < appleSize; i++)
+                {
+                    if ((apples[i].x == tail[0].x) && (apples[i].y == tail[0].y))
+                    {
+                        apples[i].ImEating = true;
+                        eatedApplesCount++;
+                        //0,1,2
+                        //Count = 1, index of first element is 0
+                        //Count = 2, index of first element is 0, index of second element is 1
+                        //tail.Count - 1 is always last element of list (tail.LastIndexOf)                        
+                        TailSegmentCoord tailSegment = new TailSegmentCoord();
+                        tailSegment.x = tail[tail.Count - 1].x;
+                        tailSegment.y = tail[tail.Count - 1].y;
+                        tail.Add(tailSegment);
+                    }
                 }
 
-                Console.CursorLeft = x;
-                Console.CursorTop = y;
-                Console.Write("X");
+                if (tail[0].x < 0)
+                {
+                    tail[0].x = widht;
+                }
 
+                if (tail[0].x > widht)
+                {
+                    tail[0].x = 0;
+                }
+
+                if (tail[0].y < 0)
+                {
+                    tail[0].y = height;
+                }
+
+                if (tail[0].y > height)
+                {
+                    tail[0].y = 0;
+                }
+
+                for (int i = 0; i < tail.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.CursorLeft = tail[i].x;
+                        Console.CursorTop = tail[i].y;
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.CursorLeft = tail[i].x;
+                        Console.CursorTop = tail[i].y;
+                        Console.Write("0");
+                    }
+                }
+
+                Console.CursorLeft = 0;
+                Console.CursorTop = 41;
+                Console.Write(eatedApplesCount);
+
+                if (eatedApplesCount == appleSize)
+                {
+                    Console.Write("Game Over");
+                    break; //while (true) is break now 
+                }
+                Thread.Sleep(100);
                 //Console.Write(key.KeyChar.ToString().ToUpper());                
             }
 

964fa920c23ee5cd9e5948a4692fbb203bd5f563
Denys Kirin <kirindenis1977@gmail.com>
2021-10-25 20:19:11 +0300

    First WPF C# application

---
+++ Sokoban/FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Title = "Button pressed";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) //event handler 
+        {
+            e.Cancel = true;
+        }
+    }
+}

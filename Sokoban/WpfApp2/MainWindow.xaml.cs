aa35c417c44e3d2816133a6253d389bc92b96714
Denys Kirin <kirindenis1977@gmail.com>
2021-11-03 19:40:40 +0200

    Snake eat apples again

--- Sokoban/WpfApp2/MainWindow.xaml.cs
+++ Sokoban/WpfApp2/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,9 +33,15 @@ namespace WpfApp2
         private int xPosition = 400;
         private int yPosition = 400;
 
-        static int appleSize = 10;
+        private int xD = 0;
+        private int yD = 0;
+
+
+        static int appleSize = 20;
 
         static List<AppleCoord> apples = new List<AppleCoord>();
+
+        public DateTime startTime = DateTime.Now;
         public MainWindow()
         {
             InitializeComponent();
@@ -62,43 +69,114 @@ namespace WpfApp2
                 a.apple.VerticalAlignment = VerticalAlignment.Top;
                 a.apple.Margin = new Thickness(a.x * 10, a.y * 10, 0, 0);
                 MainGrid.Children.Add(a.apple);
+                // X,Y-----X+10
+                // |  
+                // |    H Apple here 
+                // | 
+                // Y+10----X+10 Y+10
 
             }
+
+            Timer timer = new Timer(10); //1000ms = 1 second, 100 = 1/10 second 
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
         }
-        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+
+        //to be called each 100ms 
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            if (e.Key == Key.Up)
+            base.Dispatcher.Invoke(() =>
             {
-                yPosition-=10; 
-            }
-            else
-            if (e.Key == Key.Down)
-            {
-                yPosition += 10;
-            }
-            if (e.Key == Key.Left)
-            {
-                xPosition -= 10;
-            }
-            else
-            if (e.Key == Key.Right)
-            {
-                xPosition += 10;
-            }
-             if (xPosition > 800 || xPosition < 0 || yPosition > 450 || yPosition < 0)
+                ScoreText.Text = (DateTime.Now - startTime).ToString();
+
+                xPosition += xD;
+            yPosition += yD;
+
+                /*
+            if (xPosition > 800 || xPosition < 0 || yPosition > 450 || yPosition < 0)
             {
                 gameover.Visibility = Visibility.Visible;
                 close.Visibility = Visibility.Visible;
                 return;
             }
-            snakeHead.Margin = new Thickness(xPosition, yPosition, 0, 0);
+                */
+
+            for (int i = 0; i < appleSize; i++)
+            {//120 370
+                if (
+                   ((apples[i].x * 10 <= xPosition) && (apples[i].x * 10 + 10 >= xPosition)
+                   &&
+                   (apples[i].y * 10 <= yPosition) && (apples[i].y * 10 + 10 >= yPosition))
+                   ||
+                   ((apples[i].x * 10 <= xPosition + 20) && (apples[i].x * 10 + 10 >= xPosition + 20)
+                   &&
+                   (apples[i].y * 10 <= yPosition) && (apples[i].y * 10 + 10 >= yPosition))
+                   ||
+                   ((apples[i].x * 10 <= xPosition) && (apples[i].x * 10 + 10 >= xPosition)
+                   &&
+                   (apples[i].y * 10 <= yPosition + 20) && (apples[i].y * 10 + 10 >= yPosition + 20))
+                   ||
+                   ((apples[i].x * 10 <= xPosition + 20) && (apples[i].x * 10 + 10 >= xPosition + 20)
+                   &&
+                   (apples[i].y * 10 <= yPosition + 20) && (apples[i].y * 10 + 10 >= yPosition + 20))
+                   )
+
+
+                {
+                    apples[i].ImEating = true;
+                    apples[i].apple.Visibility = Visibility.Hidden;
+                }
+            }
+
+
+
+                snakeHead.Margin = new Thickness(xPosition, yPosition, 0, 0);
+            });
             //home task is snake is kill self by the window wall
-            //TextBlock with Text - Game Over must be show 
+            //TextBlock with Text - Game Over must be show             
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+            {
+                xD = 0;
+                yD =-3; 
+            }
+            else
+            if (e.Key == Key.Down)
+            {
+                xD = 0;
+                yD = 3;
+            }
+            if (e.Key == Key.Left)
+            {
+                yD = 0;
+                xD = -3;
+            }
+            else
+            if (e.Key == Key.Right)
+            {
+                yD = 0;
+                xD = 3;
+            }
+
+
         }
 
         private void close_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            while (true) //forever before the conditional is true
+            {
+
+            }
         }
     }
 }
